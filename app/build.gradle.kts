8c74fca5f32517207d10197208a6c13992082f07
HZY-520 <HZY-520@users.noreply.github.com>
2026-09-21 07:13:01 +0000

    chore: bump version to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.arduinobin"
         minSdk = 26
         targetSdk = 28
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
         vectorDrawables { useSupportLibrary = true }
     }
 
